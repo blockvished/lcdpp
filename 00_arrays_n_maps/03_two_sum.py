caaad0e8d669c42d4b50f7f748759a7c4d8dcd59
Lavishq <lavishq@proton.me>
2025-05-12 15:53:02 +0000

    fixed the issue

--- 00_arrays_n_maps/03_two_sum.py
+++ 00_arrays_n_maps/03_two_sum.py
@@ -7,9 +7,20 @@ def twoSum(nums, target):
         difference = target - nums[i]
 
         if difference in uniques:
+            print([uniques[difference], i])
             return [uniques[difference], i]
-        uniques[difference] = i
+        uniques[nums[i]] = i
+
 
 # well cheated in unpotimised as well
 # optimised but after a week did optimised ver.
 # can use for i, num in enumerate(nums): instead
+
+print(twoSum([2,7,11,15],9))
+print(twoSum([3,2,4],6))
+
+    # for i, num in enumerate(nums):
+    #     difference = target - num
+    #     if difference in uniques:
+    #         return [uniques[difference], i]
+    #     uniques[num] = i
