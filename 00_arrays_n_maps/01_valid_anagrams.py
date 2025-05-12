61509264e48bb0714e9b7ff7981d6c51a3380329
Lavishq <lavishq@proton.me>
2025-05-12 16:02:30 +0000

    fixed and added test

--- 00_arrays_n_maps/01_valid_anagrams.py
+++ 00_arrays_n_maps/01_valid_anagrams.py
@@ -1,9 +1,12 @@
 # https://leetcode.com/problems/valid-anagram/description/
 
-def isAnagram(self, s: str, t: str) -> bool:
+def isAnagram(s, t) -> bool:
     if len(s) != len(t):
         return False
 
     return sorted(s) == sorted(t)
 
 # googled built in python function name for sort()
+
+print(isAnagram("llo","ool"))
+print(isAnagram("llo","lol"))
--- 00_arrays_n_maps/04_group_anagrams.py
+++ 00_arrays_n_maps/04_group_anagrams.py
@@ -1,7 +1,8 @@
 # https://leetcode.com/problems/group-anagrams/
+from collections import defaultdict
 
 def groupAnagrams(strs):
-    result = dict()
+    result = defaultdict(list)
 
     for word in strs:
         key = tuple(sorted(word))
@@ -12,8 +13,12 @@ def groupAnagrams(strs):
     for value in result.values():
 
         final_result.append(value)
-        print(value)
+        # print(value)
 
     return final_result
 
 # cheated the part on how to save the key, as was getting error and rest was simple
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+print(groupAnagrams(["A"]))
+print(groupAnagrams([""]))
